1b68fe3d62340849e45d8d914062a3d05408feee
Chris Crane <chrisjcrane@gmail.com>
2022-11-02 14:11:53 -0600

    Add another variable

--- friends.rb
+++ friends.rb
@@ -1,7 +1,11 @@
-best_friend = "bil"
+best_friend = "sean"
+good_friend = "john"
 
 if best_friend == "bill"
     puts "Hey #{best_friend}!"
+elsif good_friend == "john"
+    puts "How's it going john?"
 else
     puts "Hey other person!"
 end
+
